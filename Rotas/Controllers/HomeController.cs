1280786614dc896f677f719f1007468c1b4327f5
Leivas Nascimento Dos Santos <leivas.santos@sebsa.com.br>
2021-02-09 16:45:06 -0300

    Adicionar arquivos de projeto.

---
+++ Rotas/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Rotas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Rotas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEnumerable<Noticia> noticias; // = new Noticia().TodasAsNoticias();
+        public HomeController()
+        {
+            noticias = new Noticia().TodasAsNoticias().OrderByDescending(x => x.Data);
+        }
+
+        public ActionResult Index()
+        {
+            var ultimasNoticias = noticias.Take(3);
+            var todasAsCategorias = noticias.Select(x => x.Categoria).Distinct();
+            ViewBag.Categorias = todasAsCategorias;
+
+            return View(ultimasNoticias);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Rotas/Models/Noticia.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Rotas.Models
+{
+    public class Noticia
+    {
+        public int NoticiaId { get; set; }
+        public string Titulo { get; set; }
+        public string Categoria { get; set; }
+        public DateTime Data { get; set; }
+        public string Conteudo { get; set; }
+        public IEnumerable<Noticia> TodasAsNoticias()
+        {
+            var retorno = new Collection<Noticia>
+            {
+                new Noticia {
+                    Categoria = "esporte",
+                    Data = new DateTime(2020,02,20),
+                    Conteudo = "blablabla esporte",
+                    NoticiaId = 1,
+                    Titulo = "vitória time desce"
+                },
+
+                new Noticia {
+                    Categoria = "saúde",
+                    Data = new DateTime(2020,02,20),
+                    Conteudo = "blablabla saúde",
+                    NoticiaId = 2,
+                    Titulo = "vitamina c é bom"
+                }
+            };
+
+
+            return retorno;
+           
+        }
+
+
+
+
+
+
+    }
+}
