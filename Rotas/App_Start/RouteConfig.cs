41acacc8b1635189379a7bb65132d8b116b09f87
Leivas Nascimento Dos Santos <lnsantos2@latam.stefanini.com>
2021-02-10 12:40:29 -0300

    rotas amigáveis

--- Rotas/App_Start/RouteConfig.cs
+++ Rotas/App_Start/RouteConfig.cs
@@ -12,11 +12,29 @@ namespace Rotas
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+           
+            routes.MapRoute(
+                name:"Todas as notícias",
+                url: "noticias/",
+                defaults: new { controller = "Home", action = "TodasAsNoticias" }
+                );
+
+            routes.MapRoute(
+                name: "noticia",
+                url: "noticia/",
+                defaults: new { controller = "Home", action = "MostrarNoticia" }
+                );
+
+            routes.MapRoute(
+               name: "Categoria Específica",
+               url: "noticiais/{categoria}",
+               defaults: new { controller = "Home", action = "MostrarCategoria" }
+               );
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "TodasAsNoticias", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
